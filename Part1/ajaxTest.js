272cde67c2b3374649daa58991dbe26efca449ed
Ricardo Szyfer <ricardo.szyfer@kinamic.com>
2015-02-26 02:19:34 -0200

    Added comments and simplified example.

--- Part1/ajaxTest.js
+++ Part1/ajaxTest.js
@@ -4,19 +4,19 @@
     if (window.XMLHttpRequest) {
         // create the XMLHttpRequest
         // code for IE7+, Firefox, Chrome, Opera, Safari
-        // this should be the standard nowadays.
+        // this should be the standard nowadays, actually they comprise ~80%+ of all used browsers.
         request = new XMLHttpRequest();
     } else {
-        // code for IE6, IE5
+        // code for IE6, IE5 - this should be no longer necessary. IE6 users are approximately 2% of all browser users.
         request = new ActiveXObject("Microsoft.XMLHTTP");
     }
-
-    request.onreadystatechange = function() {
-        if (request.readyState == XMLHttpRequest.DONE ) {
-            if(request.status == 200){
+    //callback function for when response is received from the server.
+    request.onreadystatechange = function () {
+        if (request.readyState == XMLHttpRequest.DONE) {
+            if (request.status == 200) {
                 document.getElementById("ajaxContent").innerHTML = request.responseText;
             }
-            else if(request.status == 400) {
+            else if (request.status == 400) {
                 alert('There was an error 400')
             }
             else {
@@ -24,7 +24,6 @@
             }
         }
     }
-
-    request.open("GET", "ajaxDiv.html", true);
+    request.open("GET", "ajaxDiv.html");
     request.send();
 }
